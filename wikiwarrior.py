d6807bf15f045517b2c2501f94dfaee90c57c189
Fabio Brito d'Araujo e Oliveira <fbaoliveira@sp.r7.com>
2014-06-13 19:12:23 -0300

    created config file

--- wikiwarrior.py
+++ wikiwarrior.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
+import json
 
+config = json.loads(open('wikiwarrior.conf.json').read())
 app = Flask(__name__)
-app_name = 'WikiWarrior'
+
+app_name = config['app_name']
 
 
 @app.route('/')
 def home():
-  return render_template('index.html', app_name=app_name)
+  response = make_response(render_template('index.html', app_name=app_name))
+  return response
 
 if __name__ == '__main__':
         app.run(debug=True)
