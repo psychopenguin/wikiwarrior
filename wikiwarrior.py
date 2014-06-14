43465062f4c7e3242377065bcf43dbc148020ed9
Fabio Brito <psychopenguin@gmail.com>
2014-06-14 02:11:43 -0300

    put sessions on redis

--- wikiwarrior.py
+++ wikiwarrior.py
@@ -4,15 +4,27 @@
 from flask import Flask, render_template, make_response, request
 import json
 from uuid import uuid4
+import redis
+
+app = Flask(__name__)
+config = json.loads(open('wikiwarrior.conf.json').read())
+app_name = config['app_name']
+redis_host = config['redis']['host']
+redis_port = config['redis']['port']
+redis_db = config['redis']['db']
+session_exp = config['game']['expiration']
+redis_conn = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db) 
 
 def unique_id():
 	uid = str(uuid4())
+	ua = request.headers.get('User-Agent')
+	referer = request.headers.get('Referer')
+	clicks = 0
+	sessison_data = {'User-Agent': ua, 'Referer': referer, 'Clicks': clicks}
+	redis_conn.set(uid, sessison_data, session_exp)
 	return uid
 
-config = json.loads(open('wikiwarrior.conf.json').read())
-app = Flask(__name__)
 
-app_name = config['app_name']
 
 
 @app.route('/')
